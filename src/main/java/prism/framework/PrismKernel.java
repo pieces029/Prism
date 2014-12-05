f6b5407b1df4c49d668be4c419937d83309f7532
Andrew Reitz <aj.reitz@gmail.com>
2014-12-05 10:07:25 -0600

    Removed constructor that would result in an NPE.
    (Fixing FindBugs Error)

--- src/main/java/prism/framework/PrismKernel.java
+++ src/main/java/prism/framework/PrismKernel.java
@@ -34,14 +34,6 @@ final public class PrismKernel
     private boolean injectContentView = true;
 
     /**
-     * Create a Graphless Application.
-     */
-    public PrismKernel()
-    {
-        this(null);
-    }
-
-    /**
      * Create an application with the specified object graphs.
      */
     public PrismKernel(GraphContext graph)
